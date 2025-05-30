d053fd1b45bf984cd5cadd35633465e6ae47bad3
Obaidullah Ch <bsf23004001@ue.edu.pk>
2025-05-30 18:59:20 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+console.log("hello")
+let name="obaid"
+let age = 23
+let isloggedIn=true
+console.table([name,age,isloggedIn])
+// number range from 0 to 2 ^ 53
+//string = written in "" or ''
+//bigInt {we mostly can use it }
+//boolean =true/false {this mean that yes or no }
+//null => its mean that it has null value mean empty 
+//symbol => its mean that this is use for uniquenesss
